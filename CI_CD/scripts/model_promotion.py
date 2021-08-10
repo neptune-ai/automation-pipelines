27adf19dfe1957f493216c8e2fc72a6a32a6d2cf
Prince Canuma <prince.gdt@gmail.com>
2021-08-10 20:54:56 +0530

    setting env var

--- CI_CD/scripts/model_promotion.py
+++ CI_CD/scripts/model_promotion.py
@@ -8,7 +8,8 @@ project_name = 'common/pytorch-integration'
 # Fetch project 
 project = neptune.get_project(
     name = project_name,
-    api_token=os.getenv("NEPTUNE_API_TOKEN"))
+    api_token=os.getenv("NEPTUNE_API_TOKEN")
+)
 
 # In-prod
 # Filter in-prod run
@@ -74,6 +75,6 @@ assert stagging_score >= prod_score, \
     f'Stagging model accuracy {round(stagging_score*100,2)} lower than threshold {round(prod_score*100,2)}%'
 
 print(f'Model from run_id={stagging_run_id} has accucy of {stagging_score*100}% that is greater than production model was promoted to production')
-os.environ.putenv('run_id', f'{stagging_run_id}')
+os.environ['run_id'] = f'{stagging_run_id}'
 
 
