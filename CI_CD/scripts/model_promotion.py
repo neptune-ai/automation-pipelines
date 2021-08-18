43b838aacc3c65f1b0d77b7226c220b3134b59f8
Prince Canuma <prince.gdt@gmail.com>
2021-08-18 16:52:14 +0530

    testing ideas

--- CI_CD/scripts/model_promotion.py
+++ CI_CD/scripts/model_promotion.py
@@ -79,7 +79,7 @@ assert stagging_score >= prod_score, \
 
 print(f'Staging model with run_id = {stagging_run_id} has accuracy of {stagging_score*100}% that is greater than the current production-model was promoted to production')
 
-os.environ.set["NEW_BEST_RUN_ID"] = stagging_run_id
+os.environ["NEW_BEST_RUN_ID"] = stagging_run_id
 command = ['sqsub', '-np', sys.argv[2], '.']
 subprocess.check_call(command)
 
